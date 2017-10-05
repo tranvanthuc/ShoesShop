5232abac8d4cb8e0496fe4ef2f448d9b06a9e602
Huy, Nguyen Hong Doan <donghogiacat@yahoo.com.vn>
2017-10-05 10:11:57 +0700

    make it better

--- config.php
+++ config.php
@@ -1,28 +1,28 @@
 <?php 
 // mysql
-return [
-  'database' => [
-    'name' => 'todos',
-    'username' => 'root',
-    'password' => 'huy',
-    'connection' => 'mysql:host=127.0.0.1',
-    'options' => [
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]
-  ]
-];
-
-// // hosting sql
 // return [
 //   'database' => [
-//     'name' => 'id3113257_neolab',
-//     'username' => 'id3113257_root',
-//     'password' => 'thuc123',
-//     'connection' => 'mysql:host=localhost',
+//     'name' => 'todos',
+//     'username' => 'root',
+//     'password' => 'huy',
+//     'connection' => 'mysql:host=127.0.0.1',
 //     'options' => [
 //       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 //     ]
 //   ]
 // ];
+
+// hosting sql
+return [
+  'database' => [
+    'name' => 'sql9.freemysqlhosting.net',
+    'username' => 'sql9197805',
+    'password' => 'cRmU75JgyF',
+    'connection' => 'mysql:host=localhost',
+    'options' => [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
+  ]
+];
 
 ?>
