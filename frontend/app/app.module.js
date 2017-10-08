b1f8db3b5e9a083c46a8926d1e863cd66876eb56
Dung Le <dungle1811@gmail.com>
2017-10-08 23:28:15 +0700

    @ Fix responsive for admin page

--- frontend/app/app.module.js
+++ frontend/app/app.module.js
@@ -10,7 +10,7 @@ import uirouter from 'angular-ui-router';
 import admin from './resources/admin';
 import routes from './app.routes';
 
-import './assets/scss/index.scss';
+import './assets/scss/admin/index.scss';
 
 angular.module('app', [
   uirouter,
