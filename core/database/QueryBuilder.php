592edcf1331cf8f8103b4d06f46fd2c99c1ebcf8
tranvanthuc <tranvanthucduytan@gmail.com>
2017-10-09 10:16:20 +0700

    + create register

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -28,9 +28,10 @@ class QueryBuilder
     $sql = sprintf(
       'insert into %s (%s) values (%s)',
       $table,
-      implode(',', array_keys($params)),
+      implode(', ', array_keys($params)),
       ':'. implode(', :', array_keys($params))
     );
+
     try {
       $stm = $this->pdo->prepare($sql);
       $stm->execute($params);
