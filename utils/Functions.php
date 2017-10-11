07497f4f873fafb057b3d35f58b4c1e57f8b810c
tranvanthuc <tranvanthucduytan@gmail.com>
2017-10-11 08:51:07 +0700

    @ Update function returnAPI-v1.0

--- utils/Functions.php
+++ utils/Functions.php
@@ -37,7 +37,7 @@ class Functions
       ];
     }
     header('Access-Control-Allow-Origin: *');
-    header('Content-type: application/json');
+    // header('Content-type: application/json');
 
     echo \json_encode($result);
   }
