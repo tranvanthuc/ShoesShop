e42d43793e9df0c2afe541803b268890cd9ea2a8
tranvanthuc <tranvanthucduytan@gmail.com>
2017-10-13 07:45:25 +0700

    @ index.php and config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ return [
   'database' => [
     'name' => 'shoesshop',
     'username' => 'shoesshop',
-    'password' => '	Abc123@@',
+    'password' => 'thuc123*',
     'connection' => 'mysql:host=mysql6.gear.host',
     'options' => [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
