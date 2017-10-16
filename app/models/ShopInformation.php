6d2c86cbfee56bedd0639f9be9869f55e5282fdd
minhthai <minhthai0124@gmail.com>
2017-10-16 14:29:26 +0700

    @ Update shop Information model, controller

--- app/models/ShopInformation.php
+++ app/models/ShopInformation.php
@@ -7,35 +7,11 @@ use app\models\Model;
 
 class ShopInformation extends Model
 {
-    public static $table = "shop_information";
+    static $table = "shop_information";
     public $description;
     public $name;
     public $address;
     public $phone;
     public $email;
 
-    // //get shop information
-    // public static function getAll()
-    // {
-    //     return App::get('database')->getAll(ShopInformation::$table);
-    // }
-    
-    // //get information of shop by id
-    // public static function getById($id)
-    // {
-    //     return App::get('database')->getById(ShopInformation::$table, $id);
-    // }
-
-    // //update information of shop by id
-    // public static function updateById($id, $data) 
-    // {
-    //     $params = [
-    //         'description' => $data['description'],
-    //         'name' => $data['name'],
-    //         'address' => $data['address'],
-    //         'phone' => $data['phone'],
-    //         'email' => $data['email']
-    //     ];
-    //     App::get('database')->updateById(ShopInformation::$table, $params, $id);
-    // }
 }
